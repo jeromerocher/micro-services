c31c36576167d821a62b37db44d227b047a79ef9
Jérôme ROCHER <jerome.rocher@hotmail.com>
2021-08-18 13:47:41 +0200

    Authentification JWT simple token

---
+++ sources/authentification/src/main/java/com/example/authentification/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.example.authentification.filters;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+	private AuthenticationManager authenticationManager;
+	
+	
+	public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+		super();
+		this.authenticationManager = authenticationManager;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+		
+		System.out.println("==> attemptAuthentication");
+		// Ici nous considérons que les informations ne sont pas envoyé dans un flux JSON (pour le POC).
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+		
+		// C'est cette méthode qui fait appel à la méthode loadUserByUsername qui va vérifier le login et mot de passe en base.
+		// En cas de succès, la méthode ci dessous successfulAuthentication est appelées
+		return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		
+		System.out.println("==> successfulAuthentication");
+		
+		User user = (User)authResult.getPrincipal();
+		
+		// Nous allons générer le token
+		Algorithm algorithmHmac256 = Algorithm.HMAC256("MA_CLE_SECRETE");
+		
+		// Génération du JWT
+		String jwt = JWT.create()
+				        .withSubject(user.getUsername())
+				        .withExpiresAt(new Date(System.currentTimeMillis()+5 * 60 *1000)) // 5 minutes
+				        .withIssuer(request.getRequestURL().toString()) // Nom de l'application qui a générée le token
+				        .withClaim("fonction", 
+				                	user.getAuthorities().stream()
+				                		                 .map(authorities -> authorities.getAuthority())
+				                		                 .collect(Collectors.toList()))
+				                   						 .sign(algorithmHmac256);
+				                   
+		response.setHeader("Authorization", jwt);
+		
+	}
+}
--- sources/authentification/src/main/java/com/example/authentification/service/SecurityConfig.java
+++ sources/authentification/src/main/java/com/example/authentification/service/SecurityConfig.java
@@ -1,31 +1,90 @@
 package com.example.authentification.service;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.hibernate.transform.ToListResultTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.authentification.entities.Utilisateur;
+import com.example.authentification.filters.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	// https://www.youtube.com/watch?v=p_PhNjBukg8&ab_channel=Sourcesdel%27InformaticienavecPr.MohamedYOUSSFI
-	// 34:11
+	@Autowired
+	private AuthentificationService authentificationService;
+	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+	protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		
+		// L'authentification est à implémenter dans une classe qui implémente l'interface UserDetailsService
+		authenticationManagerBuilder.userDetailsService(new UserDetailsService() {
+			
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				
+				Utilisateur utilisateur = authentificationService.findByUsername(username);
+				
+				List<GrantedAuthority> authorities = utilisateur.getFonctions()
+						                                  .stream()
+						                                  .map(fonction -> new SimpleGrantedAuthority(fonction.getRole()))						                                  
+						                                  .collect(Collectors.toList());
+				
+				
+				return new User(utilisateur.getUsername(), utilisateur.getPassword(), authorities);
+			}
+		});
 	}
 	
 	@Override
-	public void configure(HttpSecurity http)  throws Exception  {
-		// Désactivation du CSRF
-		http.csrf().disable();
+	public void configure(HttpSecurity httpSecurity)  throws Exception  {
+		// Désactivation du CSRF (authentification statefull par session et cookies)
+		httpSecurity.csrf().disable();		
+		
+		// Nous utilisons une authentification par toket et pas par session (stateless)
+		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 		// Nous désactivons la protection contre les frames pour afficher "h2-console" 
-		http.headers().frameOptions().disable();
+		httpSecurity.headers().frameOptions().disable();
 		
-		// Indique que toutes les requêtes ne nécessite pas une autorisations
-		http.authorizeRequests().anyRequest().permitAll();
+		// Pour afficher la mire de saisie de login et mot de passe Spring (plus besoin car authentification STATELESS)
+		//httpSecurity.formLogin();
+		
+		// Accès à la console "h2-console" sans authentification
+		httpSecurity.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+		
+		// Indique que toutes les requêtes nécessite une autorisations
+		httpSecurity.authorizeRequests().anyRequest().authenticated();
+		
+		// Ajout du filtre d'authentification (réutilisation du bean du contexte)
+		httpSecurity.addFilter(new JwtAuthenticationFilter(authenticationManagerBean()));
+	}
+	
+	/**
+	 * Ajout dans le contexte de l'objet AuthenticationManager;
+	 */
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
